474234c8cd59b76780c6c0dee12f900dafda4d07
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-04-13 11:36:02 +0300

    created a new project

---
+++ locators/TextBoxPageLocators.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+
+class TextBoxPageLocators:
+
+
+    FULL_NAME = (By.XPATH, "//input[@id='userName']")
+    EMAIL = (By.XPATHm, "//input[@id='userName']")
