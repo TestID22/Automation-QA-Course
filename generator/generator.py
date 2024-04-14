6571b5229d3a6e6f7ebc549c101a11aad4a56d99
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-04-14 12:16:26 +0300

    BasePage refactoring: put wait in separate variable

--- generator/generator.py
+++ generator/generator.py
@@ -5,8 +5,5 @@ from faker import Faker
 
 def generated_person():
     yield Person(
-        full_name =
-        email =
-        current_adress =
-        permanent_adress =
+
     )
--- pages/Base_page.py
+++ pages/Base_page.py
@@ -1,23 +1,23 @@
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 
-
+#todo: think that
 class BasePage:
     def __init__(self, driver, url):
         self.driver = driver
         self.url = url
-
+        self.wait = wait(driver,timeout=0)
     def open(self):
         self.driver.get(self.url)
 
     def element_is_visible(self, locator, timeout=0):
-        return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+        return self.wait.until(EC.visibility_of_element_located(locator))
 
     def elements_are_visible(self, locator, timeout=0):
-        return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
+        return self.wait.until(EC.visibility_of_all_elements_located(locator))
 
     def element_is_present(self, locator, timeout=0):
-        return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+        return self.wait.until(EC.presence_of_element_located(locator))
 
     def elements_are_present(self, locator, timeout=0):
         return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
