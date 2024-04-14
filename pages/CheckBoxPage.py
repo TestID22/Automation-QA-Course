45100c64399b56af4ad06c95220820b48e4280ec
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-04-14 17:31:10 +0300

    added open_full_list method, added check_random_checkbox

--- pages/CheckBoxPage.py
+++ pages/CheckBoxPage.py
@@ -1,3 +1,5 @@
+import random
+
 from locators.CheckBoxPageLocators import CheckBoxPageLocators
 from pages.Base_page import BasePage
 
@@ -6,5 +8,12 @@ class CheckBoxPage(BasePage):
 
     locators = CheckBoxPageLocators()
 
-    def open_three(self):
-        self.element_is_present(self.locators.HOME_CHECKBOX).click()
+    def open_full_list(self):
+        self.element_is_present(self.locators.EXPAND_ALL_BUTTON).click()
+
+    def check_random_checkbox(self):
+        item_list = self.elements_are_visible(self.locators.ITEM_LIST)
+        item = item_list[random.randint(1, 15)]
+        self.go_to_element(item)
+        item.click()
+        print(item.text)
