55e8d4dee179d503309961018f92250e6729631d
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-04-15 10:47:45 +0300

    added fix to get all checked titles of checked ITEMS, added new locator to collect all TITTLES  of checked items - '//*[@class='rct-icon rct-icon-check']/../../span[@class='rct-title']'

--- locators/CheckBoxPageLocators.py
+++ locators/CheckBoxPageLocators.py
@@ -4,4 +4,8 @@ from selenium.webdriver.common.by import By
 class CheckBoxPageLocators:
 
     EXPAND_ALL_BUTTON = (By.XPATH, "//button[@title='Expand all']")
-    ITEM_LIST = (By.XPATH, "//span[@class='rct-title']")
+    ITEM_LIST = (By.XPATH, "//*[@class='rct-title']")
+    CHECKED_ITEMS_LIST = (By.XPATH, "//*[@class='rct-icon rct-icon-check']")
+    TITLES_OF_CHECKED_LIST = (By.XPATH, "//*[@class='rct-icon rct-icon-check']/../../span[@class='rct-title']")
+    TITLE_ITEM = (By.XPATH, ".//ancestor::span[@class='rct-text']")
+    OUTPUT_RESULT = (By.XPATH, "//span[@class='text-success']")
--- pages/CheckBoxPage.py
+++ pages/CheckBoxPage.py
@@ -1,5 +1,7 @@
 import random
 
+from selenium.webdriver.common.by import By
+
 from locators.CheckBoxPageLocators import CheckBoxPageLocators
 from pages.Base_page import BasePage
 
@@ -11,9 +13,24 @@ class CheckBoxPage(BasePage):
     def open_full_list(self):
         self.element_is_present(self.locators.EXPAND_ALL_BUTTON).click()
 
+
     def check_random_checkbox(self):
         item_list = self.elements_are_visible(self.locators.ITEM_LIST)
-        item = item_list[random.randint(1, 15)]
-        self.go_to_element(item)
-        item.click()
-        print(item.text)
+        check_list = random.sample(range(1,15), 5)
+        for checkbox in check_list:
+            self.go_to_element(item_list[checkbox])
+            item_list[checkbox].click()
+
+    def collect_all_checked_items(self):
+        checked_items_list = self.elements_are_visible(self.locators.TITLES_OF_CHECKED_LIST)
+        checked_items = []
+        for item in checked_items_list:
+            checked_items.append(item.text)
+        return checked_items
+
+    def get_output_result(self):
+        output_result = self.elements_are_present(self.locators.OUTPUT_RESULT)
+        output_data = []
+        for res in output_result:
+            output_data.append(res.text)
+        return output_data
