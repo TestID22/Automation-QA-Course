6b7a75f7cf933aeff814058a4079510e8d459e2d
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-04-16 08:55:01 +0300

    configuration for RadioPage

--- config/Links.py
+++ config/Links.py
@@ -1,7 +1,7 @@
 
 
-
 class Links:
 
     TEXT_BOX_PAGE = "https://demoqa.com/text-box"
     CHECK_BOX_PAGE = "https://demoqa.com/checkbox"
+    RADIO_BUTTON_PAGE = "https://demoqa.com/radio-button"
