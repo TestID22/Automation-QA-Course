4a184eb345adb22b20a4039ba88101961e41989d
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-04-17 15:47:02 +0300

    added all locators

--- locators/WebTablePageLocators.py
+++ locators/WebTablePageLocators.py
@@ -4,4 +4,10 @@ from selenium.webdriver.common.by import By
 class WebTablePageLocators:
 
     ADD_BUTTON = (By.XPATH, "//button[@id='addNewRecordButton']")
-    FIRSTNAME_INPUT = (By.XPATH, "")
+    FIRSTNAME_INPUT = (By.XPATH, "//input[@id='firstName']")
+    LASTNAME_INPUT = (By.XPATH, "//input[@id='lastName']")
+    EMAIL_INPUT = (By.XPATH, "//input[@id='userEmail']")
+    AGE_INPUT = (By.XPATH, "//input[@id='age']")
+    SALARY_INPUT = (By.XPATH, "//input[@id='salary']")
+    DEPARTMENT_INPUT = (By.XPATH, "//input[@id='department']")
+    SUBMIT_BUTTON = (By.XPATH, "//button[@id='submit']")
--- tests/TestWebTablePage.py
+++ tests/TestWebTablePage.py
@@ -9,4 +9,3 @@ class TestWebTablePage(BaseTest):
     def test_create_user(self):
         self.webtable_page.open()
         self.webtable_page.create_user()
-        time.sleep(6)
