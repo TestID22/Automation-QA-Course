69e7a0bf82f395bdb8d5658c5d7f021bcbc507ae
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-04-18 08:58:06 +0300

    Updated generator - added new fields, Person: added person fields info, WebTablePage: finished crreate person test

--- generator/generator.py
+++ generator/generator.py
@@ -1,3 +1,5 @@
+import random
+
 from data.Person import Person
 from faker import Faker
 
@@ -7,7 +9,16 @@ Faker.seed()
 def generated_person():
     yield Person(
         full_name = faker_en.name(),
+        last_name=faker_en.last_name(),
         email = faker_en.email(),
+        age = random.randint(10, 89),
+        salary= random.randint(8_500, 39_000),
+        department=faker_en.job(),
         current_address = faker_en.address(),
         permanent_address = faker_en.address()
     )
+
+
+
+
+
--- pages/WebTablePage.py
+++ pages/WebTablePage.py
@@ -1,3 +1,4 @@
+from generator.generator import generated_person
 from locators.WebTablePageLocators import WebTablePageLocators
 from pages.Base_page import BasePage
 
@@ -7,4 +8,12 @@ class WebTablePage(BasePage):
     locators = WebTablePageLocators()
 
     def create_user(self):
+        person_info = next(generated_person())
         self.element_is_visible(self.locators.ADD_BUTTON).click()
+        self.element_is_visible(self.locators.FIRSTNAME_INPUT).send_keys(person_info.full_name)
+        self.element_is_visible(self.locators.LASTNAME_INPUT).send_keys(person_info.last_name)
+        self.element_is_visible(self.locators.EMAIL_INPUT).send_keys(person_info.email)
+        self.element_is_visible(self.locators.AGE_INPUT).send_keys(person_info.age)
+        self.element_is_visible(self.locators.SALARY_INPUT).send_keys(person_info.salary)
+        self.element_is_visible(self.locators.DEPARTMENT_INPUT).send_keys(person_info.department)
+        self.element_is_visible(self.locators.SUBMIT_BUTTON).click()
