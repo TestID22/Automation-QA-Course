08b78d592705b11c5007b02b3315122c20a1123d
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-04-29 18:59:25 +0300

    Configured: - upload feature with auto file generating

--- pages/UploadAndDonwloadPage.py
+++ pages/UploadAndDonwloadPage.py
@@ -1,7 +1,11 @@
+from pages.Base_page import BasePage
+from locators.UploadAndDownloadPageLocators import UploadAndDonwloadPageLocators
+from generator.generator import file_generator
 
+class UploadAndDownloadPage(BasePage):
 
+    locators = UploadAndDonwloadPageLocators()
 
-class UploadAndDownloadPage:
-
-
-    pass
+    def upload_file(self):
+        path, name = file_generator()
+        print(name, path)
