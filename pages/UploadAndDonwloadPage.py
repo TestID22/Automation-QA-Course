ea823b72c94c6904aaa57478e7fc79a80530ebfd
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-04-30 09:06:39 +0300

    Crated: - upload file test

--- pages/UploadAndDonwloadPage.py
+++ pages/UploadAndDonwloadPage.py
@@ -1,3 +1,5 @@
+import os
+
 from pages.Base_page import BasePage
 from locators.UploadAndDownloadPageLocators import UploadAndDonwloadPageLocators
 from generator.generator import file_generator
@@ -8,4 +10,12 @@ class UploadAndDownloadPage(BasePage):
 
     def upload_file(self):
         path, name = file_generator()
-        print(name, path)
+        self.element_is_visible(self.locators.UPLOAD_BUTTON).send_keys(path)
+        os.remove(path)
+        file_name = self.element_is_present(self.locators.UPLOADED_FILE).text
+        return path.split("\\")[-1], file_name.split("\\")[-1]
+
+
+    #todo: Download_file func
+    def download_file(self):
+        pass
