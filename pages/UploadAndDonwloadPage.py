5dcb7ce8aa5299f3bb4dd9fa4e3c2348521eea49
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-05-01 10:18:32 +0300

    created: - dowbload file func in UploadAndDonwloadPage.py - Download file test - via Base64

--- pages/UploadAndDonwloadPage.py
+++ pages/UploadAndDonwloadPage.py
@@ -1,4 +1,6 @@
+import base64
 import os
+import random
 
 from pages.Base_page import BasePage
 from locators.UploadAndDownloadPageLocators import UploadAndDonwloadPageLocators
@@ -15,7 +17,13 @@ class UploadAndDownloadPage(BasePage):
         file_name = self.element_is_present(self.locators.UPLOADED_FILE).text
         return path.split("\\")[-1], file_name.split("\\")[-1]
 
-
-    #todo: Download_file func
     def download_file(self):
-        pass
+        link = self.element_is_present(self.locators.DOWNLOAD_BUTTON).get_attribute("href")
+        link_b = base64.b64decode(link)
+        path_name_file = rf'd:\file{random.randint(1, 100000)}.jpg'
+        with open(path_name_file, 'wb+') as f:
+            offset = link_b.find(b'\xff\xd8')
+            f.write(link_b[offset:])
+            check_file = os.path.exists(path_name_file)
+            f.close()
+        return check_file
--- tests/TestUploadAndDownload.py
+++ tests/TestUploadAndDownload.py
@@ -9,4 +9,10 @@ class TestUploadAndDownload(BaseTest):
     def test_upload_file(self):
         self.upload_page.open()
         path, file_name = self.upload_page.upload_file()
-        assert path == file_name
+        assert path == file_name, "File has not been uploaded"
+
+
+    def test_download_file(self):
+        self.upload_page.open()
+        file_status = self.upload_page.download_file()
+        assert file_status == True, "File has not been downloaded"
