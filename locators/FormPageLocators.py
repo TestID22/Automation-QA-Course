1906456c6e6ff0243921afc5526e2772f8db6db5
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-05-04 08:47:51 +0300

    FormPage: added func - fill all fields FormPage: Added locators

--- locators/FormPageLocators.py
+++ locators/FormPageLocators.py
@@ -1,3 +1,5 @@
+import random
+
 from selenium.webdriver.common.by import By
 
 
@@ -6,4 +8,8 @@ class FormPageLocators():
     NAME_INPUT = (By.XPATH, "//input[@id='firstName']")
     LASTNAME_INPUT = (By.XPATH, "//input[@id='lastName']")
     EMAIL_INPUT = (By.XPATH, "//input[@id='userEmail']")
-    MALE_CHECKBOX = (By.XPATH, "//input[@id='gender-radio-1']")
+    GENDER = (By.XPATH, f"//label[@for='gender-radio-{random.randint(1, 3)}']")
+    MOBILE = (By.XPATH, "//input[@id='userNumber']")
+    SUBJECT = (By.XPATH, "//input[@id='subjectsInput']")
+    HOBBIES = (By.XPATH, f"//label[@for='hobbies-checkbox-{random.randint(1, 3)}']")
+    UPLOAD_FILE = (By.XPATH, "//input[@id='uploadPicture']")
--- pages/FormPage.py
+++ pages/FormPage.py
@@ -1,6 +1,8 @@
-import time
+from datetime import time
+from time import sleep
 
-from generator.generator import generated_person
+from selenium.webdriver import Keys
+from generator.generator import generated_person, file_generator
 from locators.FormPageLocators import FormPageLocators
 from pages.Base_page import BasePage
 
@@ -11,7 +13,14 @@ class FormPage(BasePage):
 
     def fill_form(self):
         person = next(generated_person())
-        self.element_is_present(self.locators.NAME_INPUT).send_keys(person.first_name)
-        self.element_is_present(self.locators.LASTNAME_INPUT).send_keys(person.last_name)
-        self.element_is_present(self.locators.EMAIL_INPUT).send_keys(person.email)
-        self.element_is_present(self.locators.MALE_CHECKBOX).click()
+        path, filename = file_generator()
+        self.element_is_visible(self.locators.NAME_INPUT).send_keys(person.first_name)
+        self.element_is_visible(self.locators.LASTNAME_INPUT).send_keys(person.last_name)
+        self.element_is_visible(self.locators.EMAIL_INPUT).send_keys(person.email)
+        self.element_is_visible(self.locators.GENDER).click()
+        self.element_is_visible(self.locators.MOBILE).send_keys(person.mobile_number) #todo: write generaTOR
+        self.element_is_visible(self.locators.SUBJECT).send_keys("Hindi")
+        self.element_is_visible(self.locators.SUBJECT).send_keys(Keys.RETURN)
+        self.element_is_visible(self.locators.HOBBIES).click()
+        self.element_is_present(self.locators.UPLOAD_FILE).send_keys(path)
+        sleep(5)
